c78cc70f62d6dc03a37e1c4014426c951ff301e2
Taras-Roboteckyi <rtvr@ukr.net>
2024-06-25 23:17:06 +0300

    changed layout files

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,4 +1,9 @@
-import React from "react";
+import "@styles/globals.css";
+
+export const metadata = {
+  title: "3D models",
+  description: "View and download free 3D models",
+};
 
 const layout = () => {
   return <div>layout</div>;
