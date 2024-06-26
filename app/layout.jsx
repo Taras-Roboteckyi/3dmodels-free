3c00f36b35fe0dfd749d99bae76b4f772b1641a1
Taras-Roboteckyi <rtvr@ukr.net>
2024-06-26 22:30:46 +0300

    added base markup and changed jsconfig settings

--- app/layout.jsx
+++ app/layout.jsx
@@ -5,8 +5,18 @@ export const metadata = {
   description: "View and download free 3D models",
 };
 
-const layout = () => {
-  return <div>layout</div>;
+const RootLayout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+        <div className="main">
+          <div className="gradient" />
+        </div>
+
+        <main className="app">{children}</main>
+      </body>
+    </html>
+  );
 };
 
-export default layout;
+export default RootLayout;
