83ea684c870eb813a043645798144966e2fdcba3
Taras-Roboteckyi <rtvr@ukr.net>
2025-04-02 22:23:51 +0300

    added TS settings

---
+++ components/Header/Menu/Menu.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  Menu,
+  MenuItem,
+  Drawer,
+  List,
+  ListItem,
+  ListItemText,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+
+export default function Header() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+  const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
+
+  const toggleDrawer = (open: boolean) => () => {
+    setDrawerOpen(open);
+  };
+
+  const openMenu = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setMenuAnchor(event.currentTarget);
+  };
+
+  const closeMenu = () => {
+    setMenuAnchor(null);
+  };
+
+  return (
+    <AppBar position="static">
+      <Toolbar className="flex justify-between">
+        {/* Мобільне меню */}
+        <IconButton
+          edge="start"
+          color="inherit"
+          onClick={toggleDrawer(true)}
+          className="lg:hidden"
+        >
+          <MenuIcon />
+        </IconButton>
+
+        {/* Заголовок */}
+        <Typography variant="h6">Мій сайт</Typography>
+
+        {/* Десктопне меню */}
+        <div className="hidden lg:flex space-x-4">
+          <MenuItem onClick={closeMenu}>Головна</MenuItem>
+          <MenuItem onClick={closeMenu}>Про нас</MenuItem>
+          <MenuItem onClick={closeMenu}>Контакти</MenuItem>
+        </div>
+
+        {/* Випадаюче меню (для іконки профілю) */}
+        <div>
+          <IconButton onClick={openMenu} color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Menu
+            anchorEl={menuAnchor}
+            open={Boolean(menuAnchor)}
+            onClose={closeMenu}
+          >
+            <MenuItem onClick={closeMenu}>Профіль</MenuItem>
+            <MenuItem onClick={closeMenu}>Налаштування</MenuItem>
+            <MenuItem onClick={closeMenu}>Вихід</MenuItem>
+          </Menu>
+        </div>
+      </Toolbar>
+
+      {/* Drawer (бічне меню) */}
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List className="w-64">
+          <ListItem button onClick={toggleDrawer(false)}>
+            <ListItemText primary="Головна" />
+          </ListItem>
+          <ListItem button onClick={toggleDrawer(false)}>
+            <ListItemText primary="Про нас" />
+          </ListItem>
+          <ListItem button onClick={toggleDrawer(false)}>
+            <ListItemText primary="Контакти" />
+          </ListItem>
+        </List>
+      </Drawer>
+    </AppBar>
+  );
+}
