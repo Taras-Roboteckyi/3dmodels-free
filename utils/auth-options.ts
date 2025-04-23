425c06df9b7a63934f590ddc6b10fd6ff58ec2c4
Taras-Roboteckyi <rtvr@ukr.net>
2025-04-23 22:16:27 +0300

    seted global type for NextAuth in next-auth.d.ts file

--- utils/auth-options.ts
+++ utils/auth-options.ts
@@ -13,4 +13,12 @@ export const authOptions: NextAuthOptions = {
     maxAge: 30 * 24 * 60 * 60, // термін життя сесії користувача 30 днів у секундах
   },
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.sub; // додає id з токена в session.user
+      }
+      return session;
+    },
+  }, // щоб отримати  user.id, а не лише email чи name
 };
