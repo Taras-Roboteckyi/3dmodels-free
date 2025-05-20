47be2a8ecaf986a42d356f19156f05e8ee466c53
Taras-Roboteckyi <rtvr@ukr.net>
2025-05-20 23:11:34 +0300

    added new page - uploadsPage

---
+++ app/user/menu/uploads/page.tsx
@@ -0,0 +1,12 @@
+import Header from "@components/Header/Header";
+import React from "react";
+
+export default function uploadsPage() {
+  return (
+    <div>
+      <Header />
+      <h1 className="text-2xl font-semibold">Мої завантаження</h1>
+      {/* Тут контент профілю */}
+    </div>
+  );
+}
