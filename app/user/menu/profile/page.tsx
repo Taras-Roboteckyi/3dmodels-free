2bf2d095596f12281ff53fd0d7060d4d3b113a46
Taras-Roboteckyi <rtvr@ukr.net>
2025-05-22 20:41:42 +0300

    added common layout in users menu

--- app/user/menu/profile/page.tsx
+++ app/user/menu/profile/page.tsx
@@ -1,10 +1,8 @@
-import Header from "@components/Header/Header";
 import React from "react";
 
 export default function ProfilePage() {
   return (
     <div>
-      <Header />
       <h1 className="text-2xl font-semibold">Мій профіль</h1>
       {/* Тут контент профілю */}
     </div>
