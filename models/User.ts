48911721e65c473a7c4aad05b30c23de5d7a912d
Taras-Roboteckyi <rtvr@ukr.net>
2025-05-26 22:24:34 +0300

    created model schema for user

---
+++ models/User.ts
@@ -0,0 +1,12 @@
+import { Schema, model, models } from "mongoose";
+
+//User.ts — для моделі користувача
+const UserSchema = new Schema({
+  email: { type: String, required: true, unique: true },
+  name: { type: String },
+  image: { type: String },
+  bio: { type: String }, // Додаткове поле, яке користувач може редагувати
+  createdAt: { type: Date, default: Date.now },
+});
+
+export const User = models.User || model("User", UserSchema);
