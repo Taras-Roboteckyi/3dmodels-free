25f84d2683079d10df692fe8b57da2778db37776
Taras-Roboteckyi <rtvr@ukr.net>
2025-06-10 20:17:40 +0300

    created new route for update image-avatar in MongoDB

---
+++ app/api/update-avatar/route.ts
@@ -0,0 +1,37 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/utils/auth-options";
+import User from "@/models/User";
+import { connectToDB } from "@/lib/database";
+
+// Оновлює поле image користувача в базі.
+
+export async function POST(req: NextRequest) {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.email) {
+    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+  }
+
+  const { image } = await req.json();
+  if (!image) {
+    return NextResponse.json({ error: "No image provided" }, { status: 400 });
+  }
+
+  try {
+    await connectToDB();
+
+    const updatedUser = await User.findOneAndUpdate(
+      { email: session.user.email },
+      { image: image },
+      { new: true }
+    );
+
+    return NextResponse.json({ user: updatedUser });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to update avatar" },
+      { status: 500 }
+    );
+  }
+}
