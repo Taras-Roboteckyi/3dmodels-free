83275338600fa8b4a9547c573eadf7965d08d810
Taras-Roboteckyi <rtvr@ukr.net>
2025-07-01 23:54:09 +0300

    An error occurred accessing Google OAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -6,3 +6,5 @@ const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };
 
 /* Перевірка реєстрації http://localhost:3000/api/auth/signin */
+
+//Не працює Google OAuth від 30.06.2025
