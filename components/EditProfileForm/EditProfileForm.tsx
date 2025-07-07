a36d3eae02b9eaf4a53e4662e2050fa13c62af42
Taras-Roboteckyi <rtvr@ukr.net>
2025-07-07 20:20:04 +0300

    added new logic for the EditProfileForm component

--- components/EditProfileForm/EditProfileForm.tsx
+++ components/EditProfileForm/EditProfileForm.tsx
@@ -18,6 +18,26 @@ const EditProfileForm = ({ initialData, onSubmit }: EditProfileFormProps) => {
   const [avatar, setAvatar] = useState<File | null>(null);
   const [previewUrl, setPreviewUrl] = useState(initialData.avatarUrl);
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const formData = new FormData(); //стандартний Web API, який дозволяє створювати об'єкт з полями форми
+    formData.append("name", name); // додає текстове поле
+    formData.append("surname", surname);
+    formData.append("description", description);
+    if (avatar) formData.append("avatar", avatar);
+
+    onSubmit(formData);
+  };
+
+  const handleAvatarChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files?.[0];
+    if (file) {
+      setAvatar(file);
+      setPreviewUrl(URL.createObjectURL(file));
+    }
+  };
+
   return <div>EditProfileForm</div>;
 };
 
