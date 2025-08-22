4a1d9955424cfc6bd84bf26ea6339355c2d82ec3
Taras-Roboteckyi <rtvr@ukr.net>
2025-08-22 22:48:36 +0300

    added PUT request to one model in models/[id]/route.ts

--- app/api/models/[id]/route.ts
+++ app/api/models/[id]/route.ts
@@ -23,3 +23,26 @@ export async function GET(
     return NextResponse.json({ error: err.message }, { status: 500 });
   }
 }
+
+// PUT /api/models/:id → оновити модель
+export async function PUT(
+  req: Request,
+  { params }: { params: { id: string } }
+) {
+  try {
+    await connectToDB();
+    const body = await req.json();
+
+    const updatedModel = await Model3D.findByIdAndUpdate(params.id, body, {
+      new: true,
+    });
+
+    if (!updatedModel) {
+      return NextResponse.json({ error: "Model not found" }, { status: 404 });
+    }
+
+    return NextResponse.json(updatedModel, { status: 200 });
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: 500 });
+  }
+}
