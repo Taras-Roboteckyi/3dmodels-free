9bfc72de3edb9de4af7bcb3876bda39772073308
Taras-Roboteckyi <rtvr@ukr.net>
2025-09-05 21:33:23 +0300

    added markup to UploadModel component

--- components/Models/UploadModel.tsx
+++ components/Models/UploadModel.tsx
@@ -23,4 +23,29 @@ export default function UploadModel() {
     const data = await res.json();
     console.log("Uploaded:", data);
   };
+  return (
+    <div className="p-4 bg-gray-800 rounded-lg">
+      <input
+        type="file"
+        accept=".glb,.gltf,.obj,.stl" // Дозволені формати файлів завантаження .glb/.gltf/.obj/.stl        onChange={(e) => setFile(e.target.files?.[0] || null)}
+      />
+      <input
+        type="text"
+        placeholder="Назва"
+        className="block mt-2 p-2 text-black"
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <textarea
+        placeholder="Опис"
+        className="block mt-2 p-2 text-black"
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button
+        onClick={handleUpload}
+        className="mt-3 px-4 py-2 bg-blue-500 rounded text-white"
+      >
+        Завантажити
+      </button>
+    </div>
+  );
 }
